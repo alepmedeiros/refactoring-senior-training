7a069aab71265d47bf0f1ba5451c84877e92c9f0
alemedeiros@bsd.com.br <alepmedeiros>
2022-06-01 14:40:04 -0300

    fix: incluido todos os fontes refatorados até o botão fechar da tela principal

---
+++ MasterRestaurante/MasterRestaurante.dpr
@@ -0,0 +1,35 @@
+program MasterRestaurante;
+
+uses
+  Forms,
+  UAjuda in 'src\pages\UAjuda.pas' {AjudaForm},
+  UConexao in 'src\pages\UConexao.pas' {ConexaoForm},
+  UFinalizadora in 'src\pages\UFinalizadora.pas' {FinalizadoraForm},
+  UFormaPagamento in 'src\pages\UFormaPagamento.pas' {FormaPagamentoForm},
+  UFormComanda in 'src\pages\UFormComanda.pas' {FormComanda},
+  UItem in 'src\pages\UItem.pas' {ItemForm},
+  ULogin in 'src\pages\ULogin.pas' {LoginForm},
+  UPeriodo in 'src\pages\UPeriodo.pas' {PeriodoForm},
+  UPesquisaComanda in 'src\pages\UPesquisaComanda.pas' {PesquisaComandaForm},
+  UPesquisaProduto in 'src\pages\UPesquisaProduto.pas' {PesquisaProdutoForm},
+  UPrincipal in 'src\pages\UPrincipal.pas' {PrincipalForm},
+  UTroco in 'src\pages\UTroco.pas' {TrocoForm},
+  UVenda in 'src\pages\UVenda.pas' {VendaForm},
+  Base in 'src\infra\db\Base.pas' {BancoDados: TDataModule},
+  UCupomNaoFiscal in 'src\utils\UCupomNaoFiscal.pas',
+  UFuncoes in 'src\utils\UFuncoes.pas',
+  UImprimeComanda in 'src\utils\UImprimeComanda.pas',
+  Base64 in 'src\utils\Base64.pas';
+
+{$R *.res}
+
+
+begin
+//  ReportMemoryLeaksOnShutdown := true;
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.Title := 'MasterRestaurante';
+  Application.CreateForm(TBancoDados, BancoDados);
+  Application.CreateForm(TPrincipalForm, PrincipalForm);
+  Application.Run;
+end.
